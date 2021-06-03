2502668c7fcbc41bb5ce6995015b5e52c3e1e97d
Caroline Joy Kolody <cj@prodical.us>
2021-06-03 11:36:58 -0500

    Added OLED Support

--- watering_kit_v2.0/watering_kit_v2.0.ino
+++ watering_kit_v2.0/watering_kit_v2.0.ino
@@ -1,11 +1,35 @@
 #include <Wire.h>
 #include "U8glib.h"
-U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_NONE);    // I2C
 #include "Wire.h"
 #include "RTClib.h"
 RTC_DS1307 RTC;
+#include <Adafruit_GFX.h>
+#include <Adafruit_SH1106.h>
+
+// Defines OLED Panel
+#define XPOS 0
+#define YPOS 1
+#define DELTAY 2
+#define OLED_RESET 4
+Adafruit_SH1106 display(OLED_RESET);
 
 int pump = 4; // Pin for Water Pump
+// All for RTC
+static unsigned long currentMillis_send = 0;
+static unsigned long  Lasttime_send = 0;
+char daysOfTheWeek[7][12] = {"Sun", "Mon", "Tues", "Wed", "Thur", "Fri", "Sat",};
+unsigned long nowtime;
+unsigned long endtime;
+unsigned long nowtimeNext;
+unsigned long nowtime1;
+unsigned long endtime1;
+unsigned long nowtimeNext1;
+unsigned long nowtime2;
+unsigned long endtime2;
+unsigned long nowtimeNext2;
+unsigned long nowtime3;
+unsigned long endtime3;
+unsigned long nowtimeNext3;
 
 // Declaration of Plants [Pump Port, Sensor Port, Time Watered, Moisture Reqd]
 int PumpPort [] = {6, 8, 9, 10};
@@ -14,22 +38,35 @@ int TimeWtr  [] = {0, 0, 0, 0};
 float MoistRqd [] = {.8, .8, .8, .8};
 
 void setup() {
+	display.begin(SH1106_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3D (for the 128x64)
 	Serial.begin(9600);
+	display.clearDisplay();
+	display.display();
 	//while(!Serial) continue;
 	Serial.println("Welcome to Plant Hub! Software v2.0.2");
 	pinMode(pump, OUTPUT);
 	digitalWrite(pump, HIGH);
 	delay(200);
 	digitalWrite(pump,LOW);
+	display.clearDisplay();
+	display.display();
+	display.setTextSize(1);
+	display.setTextColor(WHITE);
+	display.setCursor(0,0);
+	display.println("Pump Test - Passed");
+	display.display();
 	// Relay loop through test
 	Serial.print("Testing Plant Port => ");
+	display.print("Testing Plant Ports  > ");
 	for (int p = 0; PumpPort[p] != NULL; p++) {
-		if (p != 0 ) {Serial.print(", ");};
+		if (p != 0 ) {Serial.print(", "); display.print(", ");};
 		pinMode(PumpPort[p], OUTPUT);
 		digitalWrite(PumpPort[p], HIGH);
 		delay(200);
 		digitalWrite(PumpPort[p],LOW);
 		Serial.print(p);
+		display.print(p+1);
+		display.display();
 	}
 	for (int p = 0; SensPort[p] != NULL; p++) {
 
