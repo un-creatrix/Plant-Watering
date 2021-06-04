a7d3c12723be2030aaed8c56aea927831c462412
Caroline Joy Kolody <cj@prodical.us>
2021-06-04 00:39:24 -0500

    Sensor Script For Read and Mapping

--- watering_kit_v2.0/watering_kit_v2.0.ino
+++ watering_kit_v2.0/watering_kit_v2.0.ino
@@ -33,7 +33,7 @@ unsigned long nowtimeNext3;
 
 // Declaration of Plants [Pump Port, Sensor Port, Time Watered, Moisture Reqd]
 int PumpPort [] = {6, 8, 9, 10};
-word SensPort [] = {"A0", "A1", "A2", "A3"};
+char *SensPort [] = {"A0", "A1", "A2", "A3", NULL}; // A port selection for plants
 int TimeWtr  [] = {0, 0, 0, 0};
 float MoistRqd [] = {.8, .8, .8, .8};
 
@@ -42,7 +42,7 @@ void setup() {
 	Serial.begin(9600);
 	display.clearDisplay();
 	display.display();
-	//while(!Serial) continue;
+	while(!Serial) continue;
 	Serial.println("Welcome to Plant Hub! Software v2.0.2");
 	pinMode(pump, OUTPUT);
 	digitalWrite(pump, HIGH);
@@ -53,26 +53,43 @@ void setup() {
 	display.setTextSize(1);
 	display.setTextColor(WHITE);
 	display.setCursor(0,0);
-	display.println("Pump Test - Passed");
+	display.println("Testing Pump Connection");
 	display.display();
+	delay(500); // Delay to Bleed Pressure Down
+	display.clearDisplay();
 	// Relay loop through test
 	Serial.print("Testing Plant Port => ");
-	display.print("Testing Plant Ports  > ");
+	display.print("Testing Plant Ports    ");
 	for (int p = 0; PumpPort[p] != NULL; p++) {
 		if (p != 0 ) {Serial.print(", "); display.print(", ");};
 		pinMode(PumpPort[p], OUTPUT);
 		digitalWrite(PumpPort[p], HIGH);
-		delay(200);
-		digitalWrite(PumpPort[p],LOW);
+		delay(100);
 		Serial.print(p);
 		display.print(p+1);
 		display.display();
+		digitalWrite(PumpPort[p],LOW);
 	}
-	for (int p = 0; SensPort[p] != NULL; p++) {
-
+	Serial.println();
+	delay(500);
+	display.clearDisplay();
+	// Sensor loop through test
+	Serial.print("Testing Plant Sensor => ");
+	display.print("Testing Plant Sensors ");
+	display.display();
+	for (int p = 0; SensPort[p] != NULL ; p++) {
+		if (p != 0 ) {Serial.print(", "); display.print("\n");};
+		delay(200);
+		Serial.print(SensPort[p]);
+		display.print(SensPort[p]);
+		Serial.print(" - ");
+		display.print(" - ");
+		float reading = analogRead(SensPort[p]);
+		int reading_mapped =map(reading,590,360,0,1000);
+		Serial.print(reading_mapped);
+		display.print(reading_mapped);
+		display.display();
 	}
-
-
 	Serial.print("\n");
 }
 void loop() {
